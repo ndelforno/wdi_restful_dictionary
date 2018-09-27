0c4b96e0832c3b639b270abe3594563d2c2beaab
Ndelforno <nicolas.delforno@gmail.com>
2018-09-27 17:03:53 -0400

    exercise done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,27 +5,49 @@ class EntriesController < ApplicationController
   end
 
   def show
-
+    @entry= Entry.find(params[:id])
   end
 
   def new
-
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
     redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @entry = Entry.find(params[:id])
   end
 
   def update
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
     redirect_to entry_url(params[:id])
+    else
+      render :edit
+    end
   end
 
   def destroy
-    redirect_to entries_url  
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_url
   end
 
 end
